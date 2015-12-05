4301b7d71802ba53938603277f529ac88e64be52
Marco Glauser <marco.glauser@gmail.com>
2015-12-05 16:32:20 +0100

    some styling

--- app/events/events.js
+++ app/events/events.js
@@ -2,7 +2,7 @@ define([], function () {
     function eventsController($scope,eventsService){
         eventsService.list().then(function (events){
             $scope.events = events
-
+            console.log(events)
         })
     }
 
