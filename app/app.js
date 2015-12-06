9abb7a8e977b2ddecd5aa1c0cbbf303b74212e56
Marco Glauser <marco.glauser@gmail.com>
2015-12-06 15:21:47 +0100

    small changes

--- app/app.js
+++ app/app.js
@@ -1,56 +1,75 @@
-define(['angular','events/events','events/eventsService', 'guests/showGuests', 'guests/guestsService', 'guests/addGuest', 'events/createEvent', 'components/services/UUIDService', 'angular-resource','angular-route'],
-    function (angular, eventsController, eventsService, guestsController, guestService, addGuestController,createEventController,uuidService) {
+define(
+    [
+        'angular',
+        'events/events',
+        'events/eventsService',
+        'guests/showGuests',
+        'guests/guestsService',
+        'guests/addGuest',
+        'events/createEvent',
+        'components/services/UUIDService',
+        'angular-resource','angular-route'
+    ],
+    function (
+        angular,
+        eventsController,
+        eventsService,
+        guestsController,
+        guestService,
+        addGuestController,
+        createEventController,
+        uuidService
+    ) {
 
-    var eventManager = angular.module('eventManager', ['ngRoute','ngResource']);
+        var eventManager = angular.module('eventManager', ['ngRoute','ngResource']);
 
-    eventsService.$inject = ['$resource'];
-    eventManager.factory('eventsService',eventsService);
+        eventsService.$inject = ['$resource'];
+        eventManager.factory('eventsService',eventsService);
 
-    eventManager.factory('uuidService',uuidService);
+        eventManager.factory('uuidService',uuidService);
 
-    eventsController.$inject=['$scope','eventsService'];
-    eventManager.controller('eventsController',eventsController);
+        eventsController.$inject=['$scope','eventsService'];
+        eventManager.controller('eventsController',eventsController);
 
-    createEventController.$inject=['$scope','eventsService','uuidService'];
-    eventManager.controller('createEventController',createEventController);
+        createEventController.$inject=['$scope','eventsService','uuidService'];
+        eventManager.controller('createEventController',createEventController);
 
-    // guests related stuff
-    guestService.$inject = ['$resource'];
-    eventManager.factory('guestService', guestService);
+        // guests related stuff
+        guestService.$inject = ['$resource'];
+        eventManager.factory('guestService', guestService);
 
-    guestsController.$inject=['$scope','guestService', '$routeParams'];
-    eventManager.controller('guestsController',guestsController);
+        guestsController.$inject=['$scope','guestService', '$routeParams'];
+        eventManager.controller('guestsController',guestsController);
 
-    addGuestController.$inject=['$scope','guestService', '$routeParams'];
-    eventManager.controller('addGuestController',addGuestController);
+        addGuestController.$inject=['$scope','guestService', '$routeParams'];
+        eventManager.controller('addGuestController',addGuestController);
 
-    eventManager.config(['$routeProvider', function($routeProvider) {
-        $routeProvider
-            .when('/', {
-                templateUrl: 'events/events.html',
-                controller: 'eventsController'
-            })
-            .when('/events/create', {
-                templateUrl: 'events/createEvent.html',
-                controller: 'createEventController'
-            })
-            .when('/events/:eventId', {
-                templateUrl: 'guests/showGuests.html',
-                controller: 'guestsController'
-            })
-            .when('/events/:eventId/guests', {
-                templateUrl: 'guests/showGuests.html',
-                controller: 'guestsController'
-            })
-            .when('/events/:eventId/addGuest', {
-                templateUrl: 'guests/addGuest.html',
-                controller: 'addGuestController'
-            })
-            .otherwise({
-                redirectTo: '/'
-            });
-    }]);
-
-    return eventManager;
+        eventManager.config(['$routeProvider', function($routeProvider) {
+            $routeProvider
+                .when('/', {
+                    templateUrl: 'events/events.html',
+                    controller: 'eventsController'
+                })
+                .when('/events/create', {
+                    templateUrl: 'events/createEvent.html',
+                    controller: 'createEventController'
+                })
+                .when('/events/:eventId', {
+                    templateUrl: 'guests/showGuests.html',
+                    controller: 'guestsController'
+                })
+                .when('/events/:eventId/guests', {
+                    templateUrl: 'guests/showGuests.html',
+                    controller: 'guestsController'
+                })
+                .when('/events/:eventId/addGuest', {
+                    templateUrl: 'guests/addGuest.html',
+                    controller: 'addGuestController'
+                })
+                .otherwise({
+                    redirectTo: '/'
+                });
+        }]);
+        return eventManager;
 });
 
